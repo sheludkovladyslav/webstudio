6e8078597d48c80ec18882a697e31edc12386285
Vladyslav Sheludko <vladsh2103@gmail.com>
2023-09-17 22:46:53 -0400

    готово

---
+++ js/choose.js
@@ -0,0 +1,4 @@
+document.getElementById("web").addEventListener("click", function () {
+  document.getElementById("portfolio-card").classList.add("remove");
+  document.getElementById("website").classList.add("show");
+});
