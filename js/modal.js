dd2556ae6caa15f746e6b8fbbb610db064f61cac
Vladyslav Sheludko <vladsh2103@gmail.com>
2023-10-08 16:31:30 -0400

    Дз готове

--- js/modal.js
+++ js/modal.js
@@ -3,13 +3,15 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    backdrop: document.querySelector("[data-backdrop]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.backdrop.classList.toggle("is-hidden"); // Додаємо або забираємо клас до бекдропу, щоб створити css перехід
+    refs.modal.classList.toggle("effect"); // Додаємо клас або забираємо до модального вікна, щоб створити css перехід
     document.body.classList.toggle("no-scroll");
   }
 })();
